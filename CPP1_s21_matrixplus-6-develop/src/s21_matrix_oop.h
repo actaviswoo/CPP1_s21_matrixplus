811b00505ad44ea580b9dda67f4dcfe2ae91c9ca
timyr <109249381+actaviswoo@users.noreply.github.com>
2023-07-29 19:00:22 +0300

    Add files via upload

---
+++ CPP1_s21_matrixplus-6-develop/src/s21_matrix_oop.h
@@ -0,0 +1,64 @@
+#ifndef CPP1_S21_MATRIXPLUS_SRC_S21_MATRIX_OOP_H
+#define CPP1_S21_MATRIXPLUS_SRC_S21_MATRIX_OOP_H
+
+#include <cmath>
+#include <iostream>
+
+#define EPS 1e-7
+
+class S21Matrix {
+ public:
+  //  constructors and destructors
+  S21Matrix() = default;
+  S21Matrix(int rows, int cols);
+  S21Matrix(const S21Matrix& other);
+  S21Matrix(S21Matrix&& other);
+  ~S21Matrix();
+  // accessor and mutator
+  int GetRows() const;
+  int GetCols() const;
+  void SetRows(int rows);
+  void SetCols(int cols);
+  // operations
+  bool EqMatrix(const S21Matrix& other) const;
+  void SumMatrix(const S21Matrix& other);
+  void SubMatrix(const S21Matrix& other);
+  void MulNumber(const double num);
+  void MulMatrix(const S21Matrix& other);
+  S21Matrix Transpose() const;
+  S21Matrix CalcComplements() const;
+  double Determinant() const;
+  S21Matrix InverseMatrix() const;
+  // operators
+  S21Matrix operator+(const S21Matrix& other);
+  S21Matrix operator-(const S21Matrix& other);
+  S21Matrix operator*(const S21Matrix& other);
+  S21Matrix operator*(const double num);
+  S21Matrix& operator=(const S21Matrix& other);
+  S21Matrix operator=(S21Matrix&& other);
+  S21Matrix operator+=(const S21Matrix& other);
+  S21Matrix operator-=(const S21Matrix& other);
+  S21Matrix operator*=(const S21Matrix& other);
+  S21Matrix operator*=(const double num);
+  bool operator==(const S21Matrix& other) const;
+  double& operator()(int i, int j);
+  const double& operator()(int i, int j) const;
+
+ private:
+  int rows_ = 0, cols_ = 0;
+  double** matrix_ = nullptr;
+  void RemoveMatrix();
+  void SetNull();
+  void CopyMatrix(const S21Matrix& other);
+  void CreateMatrix();
+  void CreateMinor(int i, int j, const S21Matrix& minor) const;
+  void ExceptionMatrixDoesntExist() const;
+  void ExceptionMatrixIsNotSquare() const;
+  void ExceptionMatrixDoesntExist(const S21Matrix& other) const;
+  void ExceptionDifferentDimensions(const S21Matrix& other) const;
+  void ExceptionDifferentDimensionsForMul(const S21Matrix& other) const;
+  void ExceptionOutOfRange(int i, int j) const;
+  void ExceptionDeterminant(double determinant) const;
+};
+
+#endif  // CPP1_S21_MATRIXPLUS_SRC_S21_MATRIX_OOP_H
